5ae73cdeaa77b0350a8ab38cb74a192eef70c8fe
RJ Bergeron <rbergeron@palantir.com>
2016-03-05 12:33:36 -0800

    add missing dot

--- bitlbee/build.sh
+++ bitlbee/build.sh
@@ -3,4 +3,4 @@
 myver=$(git log -1 --format=%h)
 
 # create bitlbee image with a tag of the current repo, and the date.
-docker build -t bitlbee-c7-${myver}-$(date +%s)
+docker build -t bitlbee-c7-${myver}-$(date +%s) .
