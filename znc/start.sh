2ec39bc1356ec899cb68257a2924e151f3938354
RJ Bergeron <rbergeron@palantir.com>
2016-03-12 11:48:36 -0800

    have a frustrating argument with su

--- znc/start.sh
+++ znc/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # start znc using su - "-d /znc -f" are args to znc
-/usr/bin/su --session-command /usr/bin/znc "-d /znc -f" znc
+/usr/bin/su --session-command "/usr/bin/znc -d /znc -f" znc
