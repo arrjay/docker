c63f154186e2f10ee020edb83d796f77fea789fb
RJ Bergeron <rbergeron@palantir.com>
2016-03-13 12:38:51 -0700

    psyznc, really...

--- psybnc/build.sh
+++ psybnc/build.sh
@@ -3,7 +3,7 @@
 myver=$(git log -1 --format=%h)
 
 # create znc image with a tag of the current repo, and the date.
-docker build -t arrjay/cc:psyznc-c7-${myver}-$(date +%s) .
+docker build -t arrjay/cc:psybnc-c7-${myver}-$(date +%s) .
 
 # tests would go here...
 
